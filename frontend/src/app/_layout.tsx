32179ed4bb6cb916e12d6209c07904fa139eab83
SNeranjanaRajapaksha <snrajapaksha@std.appsc.sab.ac.lk>
2026-09-21 23:09:13 +0530

    update _layout

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -3,19 +3,24 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+
+      // Public Routes
       <Stack.Screen name="index" />
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
 
+      // User Routes
       <Stack.Screen name='user/dashboard' />
       <Stack.Screen name="user/create-complaint" />
       <Stack.Screen name="user/complaints" />
       <Stack.Screen name="user/complaint/[id]" />
       <Stack.Screen name="user/profile" />
 
+      // Officer Routes
       <Stack.Screen name="officer/dashboard" />
       <Stack.Screen name="officer/complaint/[id]" />
 
+      // Admin Routes
       <Stack.Screen name="admin/dashboard" />
       <Stack.Screen name="admin/complaint/[id]" />
       <Stack.Screen name="admin/complaints" />
